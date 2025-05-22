1de4833924ed67547ccbc5008b2cc16e81cedcff
Francisco Cunha <francisccdn@gmail.com>
2025-05-22 11:52:10 +0200

    Delete boundary as edges

--- prototype/boundaries.py
+++ prototype/boundaries.py
@@ -5,7 +5,6 @@ from visualizations import show_scaled, visualize_mask
 
 
 def mask_from_boundary(boundary: np.ndarray):
-    # TODO adapt for boundary == edges
     # We start with the boundary where alpha = 255 means available space
     return boundary[:, :, 3] == 255
 
@@ -24,24 +23,6 @@ def pad_mask(mask: np.ndarray, padding_size: int):
             padded[y, x] = window.any()
 
     return padded
-
-
-def canny_test():
-    img = cv2.imread('data/trunk_unpatterned.png', cv2.IMREAD_UNCHANGED)
-    assert img is not None, "file could not be read, check with os.path.exists()"
-    show_scaled("Original", img, 4)
-
-    # for min in range(0, 1000, 100):
-    #     for max in range(min, 1000, 100):
-    #         edges = cv2.Canny(img, min, max)
-    #         show_scaled(f"Edges {min} - {max}", edges, 4)
-
-    for min, max in [(0, 0), (200, 300), (100, 400), (0, 200), (0, 100), (0, 300), (300, 500), (300, 900)]:
-        edges = cv2.Canny(img, min, max)
-        show_scaled(f"Edges {min} - {max}", edges, 8)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
